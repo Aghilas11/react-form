adb19077d0a7b52c5407d984aee229564f1ccab7
AGHILAS <halouane.aghilas@yahoo.com>
2023-07-01 16:55:33 +0200

    un formulaire avec react a utiliser sur tout les sites vitrine

---
+++ README.md
@@ -0,0 +1,5 @@
+Service mail : service_eefqv33
+template mail : template_xsf3enb
+
+Public Key : RiConrPTyYHRZ-A7l
+Private Key : K615HJX6I9op25dT5uf9n
---
+++ src/FormTemplate.js
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const FormTemplate = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const formMess = document.querySelector(".form-message");
+
+    emailjs
+      .sendForm(
+        "service_eefqv33",
+        "template_xsf3enb",
+        form.current,
+        process.env.REACT_APP_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          form.current.reset();
+          formMess.innerHTML = " <p class = 'succes'> Message envoyé </p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2000);
+        },
+        (error) => {
+          console.log(error.text);
+          formMess.innerHTML =
+            " <p class = 'error'> Une erreur s'est produite veuillez réessayer </p>";
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2000);
+        }
+      );
+  };
+
+  return (
+    <div className="form-container">
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" required autoComplete="off" />
+        <label>Email</label>
+        <input type="email" name="email" required autoComplete="off" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Envoyer" required autoComplete="off" />
+      </form>
+      <div className="form-message"></div>
+    </div>
+  );
+};
+
+export default FormTemplate;
